8b7239e91d24ea5c8a938b601ad29c703c8dde1d
Mehdi Hajikhani <mhaji007@fiu.edu>
2022-11-12 14:58:01 -0800

    Fix typo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import "./Sidebar.css"
-import Logo from "../../images/logo.png"
+import React from "react";
+import "./Sidebar.css";
+import Logo from "../../images/logo.png";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-        {/* Logo */}
-        <div className="logo">
-            <img src={Logo} alt="" />
-            <span>D<span>A</span>SH</span>
-        </div>
+      {/* Logo */}
+      <div className="logo">
+        <img src={Logo} alt="" />
+        <span>
+          D<span>A</span>SH
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
