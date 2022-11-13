0e415e73c417d22d7e5357c9b04f4eb2782d0bc5
Mehdi Hajikhani <mhaji007@fiu.edu>
2022-11-13 15:37:54 -0800

    Render cards dynamically

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import "./Cards.css"
+import React from "react";
+import "./Cards.css";
+import { cardsData } from "../../Data/Data";
 
-function Cards() {
+import Card from "../Card/Card";
+
+const Cards = () => {
   return (
-    <div>Cards</div>
-  )
-}
+    <div className="Cards">
+      {cardsData.map((card, id) => {
+        return (
+          <div className="parentContainer" key={id}>
+            <Card
+              title={card.title}
+              color={card.color}
+              barValue={card.barValue}
+              value={card.value}
+              png={card.png}
+              series={card.series}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Cards
+export default Cards;
