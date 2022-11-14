5d0c88303731e672f7f9b34230930646c50f875c
Mehdi Hajikhani <mhaji007@fiu.edu>
2022-11-14 14:42:43 -0800

    Create component for card in collapsed state

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./Card.css";
 import { motion, AnimateSharedLayout } from "framer-motion";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 // parent Card
 
@@ -10,9 +12,38 @@ const Card = (props) => {
     // AnimatedSharedLayout provides extra frames for cards in expanded and collapsed state
     // We just need to define the two different layout
     <AnimateSharedLayout>
-      {expanded ? <ExpandedCard /> : <CompactCard />}
+      {expanded ? "Expanded Card" : <CompactCard param={props} />}
     </AnimateSharedLayout>
   );
 };
 
 export default Card;
+
+// Compact Card
+function CompactCard({ param, setExpanded }) {
+  const Png = param.png;
+  return (
+    <motion.div
+      className="CompactCard"
+      style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+      }}
+      layoutId="expandableCard"
+      onClick={setExpanded}
+    >
+      <div className="radialBar">
+        <CircularProgressbar
+          value={param.barValue}
+          text={`${param.barValue}%`}
+        />
+        <span>{param.title}</span>
+      </div>
+      <div className="detail">
+        <Png />
+        <span>${param.value}</span>
+        <span>Last 24 hours</span>
+      </div>
+    </motion.div>
+  );
+}
