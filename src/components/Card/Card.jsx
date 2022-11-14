c6b2f5c65a83a14ddfadf577500ab560f6a387e7
Mehdi Hajikhani <mhaji007@fiu.edu>
2022-11-13 16:21:18 -0800

    Set up logic for expanded vs collapsed state via shared layout from framer motion

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
+import { motion, AnimateSharedLayout } from "framer-motion";
 
-function Card() {
-  return <div>Card</div>;
-}
+// parent Card
+
+const Card = (props) => {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    // AnimatedSharedLayout provides extra frames for cards in expanded and collapsed state
+    // We just need to define the two different layout
+    <AnimateSharedLayout>
+      {expanded ? <ExpandedCard /> : <CompactCard />}
+    </AnimateSharedLayout>
+  );
+};
 
 export default Card;
