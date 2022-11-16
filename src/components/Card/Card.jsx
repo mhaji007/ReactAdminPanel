e11d28dbab830c7afee1b5d0d9ed33809653affd
Mehdi Hajikhani <mhaji007@fiu.edu>
2022-11-15 16:18:18 -0800

    Create component for card in expanded state

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,8 +3,10 @@ import "./Card.css";
 import { motion, AnimateSharedLayout } from "framer-motion";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import { UilTimes } from "@iconscout/react-unicons";
+import Chart from "react-apexcharts";
 
-// parent Card
+// Parent Card
 
 const Card = (props) => {
   const [expanded, setExpanded] = useState(false);
@@ -12,7 +14,11 @@ const Card = (props) => {
     // AnimatedSharedLayout provides extra frames for cards in expanded and collapsed state
     // We just need to define the two different layout
     <AnimateSharedLayout>
-      {expanded ? "Expanded Card" : <CompactCard param={props} />}
+      {expanded ? (
+        <ExpandedCard param={props} setExpanded={() => setExpanded(false)} />
+      ) : (
+        <CompactCard param={props} setExpanded={() => setExpanded(true)} />
+      )}
     </AnimateSharedLayout>
   );
 };
@@ -44,6 +50,80 @@ function CompactCard({ param, setExpanded }) {
         <span>${param.value}</span>
         <span>Last 24 hours</span>
       </div>
+    </motion.div>
+  );
+}
+
+// Expanded Card
+function ExpandedCard({ param, setExpanded }) {
+  const data = {
+    options: {
+      chart: {
+        type: "area",
+        height: "auto",
+      },
+
+      dropShadow: {
+        enabled: false,
+        enabledOnSeries: undefined,
+        top: 0,
+        left: 0,
+        blur: 3,
+        color: "#000",
+        opacity: 0.35,
+      },
+
+      fill: {
+        colors: ["#fff"],
+        type: "gradient",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "smooth",
+        colors: ["white"],
+      },
+      tooltip: {
+        x: {
+          format: "dd/MM/yy HH:mm",
+        },
+      },
+      grid: {
+        show: true,
+      },
+      xaxis: {
+        type: "datetime",
+        categories: [
+          "2018-09-19T00:00:00.000Z",
+          "2018-09-19T01:30:00.000Z",
+          "2018-09-19T02:30:00.000Z",
+          "2018-09-19T03:30:00.000Z",
+          "2018-09-19T04:30:00.000Z",
+          "2018-09-19T05:30:00.000Z",
+          "2018-09-19T06:30:00.000Z",
+        ],
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      className="ExpandedCard"
+      style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+      }}
+      layoutId="expandableCard"
+    >
+      <div style={{ alignSelf: "flex-end", cursor: "pointer", color: "white" }}>
+        <UilTimes onClick={setExpanded} />
+      </div>
+      <span>{param.title}</span>
+      <div className="chartContainer">
+        <Chart options={data.options} series={param.series} type="area" />
+      </div>
+      <span>Last 24 hours</span>
     </motion.div>
   );
 }
